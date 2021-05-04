cb51831d3fb20913a162b1cdf35dc003fecb3c9e
Fuad Aliah <eye.design17@yahoo.com>
2021-05-04 07:32:58 +0300

    update

--- src/Views/Loader/Loader.js
+++ src/Views/Loader/Loader.js
@@ -1,40 +1,35 @@
-import React, { Component } from 'react';
-import CountUp from 'react-countup';
+import React, { Component } from "react";
+import CountUp from "react-countup";
 
-import './Loader.scss'
+import "./Loader.scss";
 
 class Loader extends Component {
+  state = {
+    end: 0,
+    hide: false,
+  };
 
-    state = {
-        end: 0,
-        hide: false
-    }
+  componentDidMount = () => {
+    this.setState({
+      end: new Date().getFullYear(),
+    });
 
-    componentDidMount = () => {
-        this.setState({
-            end: 2020
-        })
+    setTimeout(() => {
+      this.setState({ hide: true });
+    }, 3000);
+  };
 
-        setTimeout(() => {
-            this.setState({ hide: true })
-        }, 3000);
-    }
-
-    render() {
-        return (
-            <React.Fragment >
-                <section className={this.state.end === 2020 ? 'loader hide' : ' loader show'} style={this.state.hide ? ({ display: 'none' }) : null}>
-                    <CountUp
-                        start={1990}
-                        end={this.state.end}>
-                        {({ countUpRef }) => (
-                            <p className={this.state.end === 2020 ? 'number hidden' : 'number shown'} ref={countUpRef}></p>
-                        )}
-                    </CountUp>
-                </section>
-            </React.Fragment >
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <section className={this.state.end > 2020 ? "loader hide" : " loader show"} style={this.state.hide ? { display: "none" } : null}>
+          <CountUp start={1990} end={this.state.end}>
+            {({ countUpRef }) => <p className={this.state.end > 2020 ? "number hidden" : "number shown"} ref={countUpRef}></p>}
+          </CountUp>
+        </section>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Loader;
